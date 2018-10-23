b335d1429ce61f2efcf0dd67b3cda73b00a84823
aarond203 <15148602@studentmail.ul.ie>
2018-10-23 12:05:02 +0100

    Registration testing
    Testing user registration to test MS SQL database

---
+++ fypProjectWebApp/fypProjectWebApp/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using fypProjectWebApp.Models;
+
+namespace fypProjectWebApp.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        [HttpGet]
+        public ActionResult AddOrEdit(int i = 0)
+        {
+            User mod = new User();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(User mod)
+        {
+            using (TestDbModel db = new TestDbModel())
+            {
+                if (db.Users.Any(x => x.idNumber == mod.idNumber))
+                {
+                    ViewBag.DuplicateMessage = "Student ID already exists. Please try again";
+                    return View("AddOrEdit", mod);
+                }
+                db.Users.Add(mod);
+                db.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Successful";
+            return View("AddOrEdit", new User());
+        }
+    }
+}
